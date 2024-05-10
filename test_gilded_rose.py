a7d3c9f32f14320b87a7f2d809856e5e44efee54
poolan <raulpoolan@gmail.com>
2024-05-10 19:59:16 +0300

    Removed GildedRose class, update_quality now Iterable, fixed firt test

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import Item, update_quality
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("foo", items[0].name)
+    def test_item_doesnt_change_name(self):
+        item = Item("foo", 0, 0)
+        update_quality([item])
+        self.assertEqual("foo", item.name)
 
         
 if __name__ == '__main__':
